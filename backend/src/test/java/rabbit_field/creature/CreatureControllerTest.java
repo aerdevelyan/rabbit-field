d8eb8ad1c63a0f0d46977ac6eb25a55a245c3422
Armen Erdevelyan <armen.erdevelyan@gmail.com>
2018-06-21 23:17:32 +0400

    fixed CreatureControllerTest

--- backend/src/test/java/rabbit_field/creature/CreatureControllerTest.java
+++ backend/src/test/java/rabbit_field/creature/CreatureControllerTest.java
@@ -4,6 +4,7 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.Set;
 import java.util.concurrent.BlockingQueue;
@@ -14,6 +15,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import rabbit_field.Field;
@@ -28,24 +30,32 @@ public class CreatureControllerTest {
 	MasterMind masterMind = mock(MasterMind.class);
 	DelayQueue<PendingProcess> decidedActions = new DelayQueue<>();
 	BlockingQueue<StatusUpdate> statusUpdates = new LinkedBlockingQueue<>();
-	Field field = new Field(); //mock(Field.class);
+	
+	@Before
+	public void prepare() {
+		when(creature.getSpeed()).thenReturn(4f);
+		when(creature.toString()).thenReturn("test_creature");
+	}
 	
 	@Test
-	public void testCompleter() throws InterruptedException {
+	public void decidedActionsWatcherCreatesStatusUpdate() throws Exception {
 		Future<Action> completedAction = CompletableFuture.completedFuture(Action.NONE);
 		DecidedActionsWatcherTask completerTask = new DecidedActionsWatcherTask(decidedActions, statusUpdates);
 		exec.execute(completerTask);
-		PendingProcess process = new PendingProcess(creature, completedAction, System.currentTimeMillis() + 400);
+		PendingProcess process = new PendingProcess(creature, completedAction, System.currentTimeMillis());
 		decidedActions.put(process);
 		completerTask.shutdown();
 		exec.shutdown();
 		exec.awaitTermination(5, SECONDS);
-//		verify(creature).actionIsDecided(Action.NONE);
+		System.out.println("delay time: " + (System.currentTimeMillis() - process.timeStarted));
+		assertThat(statusUpdates).hasSize(1).extracting(StatusUpdate::getCreature).containsOnly(creature);
+		assertThat(statusUpdates).extracting(StatusUpdate::getAction).containsOnly(completedAction.get());
 	}
 	
 	
 	@Test
 	public void updateFulfillment_NewCreature() throws Exception {
+		Field field = new Field(); //mock(Field.class);
 		creature = new Rabbit("test_cr", field);
 		statusUpdates.add(new StatusUpdate(StatusType.NEW, creature, Action.NONE));
 		UpdatesFulfillmentTask uft = new UpdatesFulfillmentTask(statusUpdates, field, masterMind);
@@ -59,16 +69,17 @@ public class CreatureControllerTest {
 	
 	@Test
 	public void updateFulfillment_AccomplishAction() throws Exception {
+		Field field = new Field();
 		creature = new Rabbit("test_cr", field);
-		field.findRandomFreeCell().addObject(creature);
+		field.findCellBy(new Position(0, 0)).addObject(creature);
 		Position origPosition = creature.getPosition();
 		int origAge = creature.getAge();
 		int origStamina = creature.getStamina();
-		statusUpdates.add(new StatusUpdate(StatusType.DECIDED, creature, new Action.Move(Direction.NORTH))); //TODO check movement possible
+		statusUpdates.add(new StatusUpdate(StatusType.DECIDED, creature, new Action.Move(Direction.SOUTH)));
 		UpdatesFulfillmentTask uft = new UpdatesFulfillmentTask(statusUpdates, field, masterMind);
 		uft.shutdown();
 		uft.run();
-		assertThat(creature.getPosition().getVpos()).isEqualTo(origPosition.getVpos() - 1);
+		assertThat(creature.getPosition().getVpos()).isEqualTo(origPosition.getVpos() + 1);
 		assertThat(creature.getAge()).isEqualTo(origAge + 1);
 		assertThat(creature.getStamina()).isEqualTo(origStamina - 1);
 		verify(masterMind).letCreatureThink(creature);
